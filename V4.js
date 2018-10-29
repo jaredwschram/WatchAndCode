073e51946b5a910d714f8b4edd41b7307dce5358
Jared Schram <jaredwschram@gmail.com>
2018-10-29 19:19:21 -0400

    Completed V4 of Watch and Code

---
+++ V4.js
@@ -0,0 +1,27 @@
+var todoList  = {
+    todos: [],
+    displayTodos: function(){
+        console.log('My todos: ', this.todos);
+    },
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            isCompleted: false,
+
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position){
+        this.todos.splice[position, 1];
+        this.displayTodos();
+    },
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.isCompleted = !todo.isCompleted;
+        this.displayTodos();
+    }
+};
